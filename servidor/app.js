11089a5cc19e3d4d52a888f884a5e113408018bd
jose diaz <jdiazj3@gmail.com>
2018-07-31 19:40:40 -0500

    add coments javi

--- servidor/app.js
+++ servidor/app.js
@@ -27,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/asdfs',estudianteRouter)
+app.use('/asdfs',estudianteRouter);
+app.use('/ciudadano',ciudadanoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
