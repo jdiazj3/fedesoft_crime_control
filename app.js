4a4a700bf859d6556b5a7ff3fb97d2c739834554
rolog12 <rolog12@gmail.com>
2018-08-21 20:20:41 -0500

    add cors

--- app.js
+++ app.js
@@ -6,41 +6,36 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var estudianteRouter=require('./routes/estudianteRouter');
-var ciudadanoRouter=require('./routes/ciudadanoRouter');
-var comunitarioRouter=require('./routes/comunitarioRouter');
-var agenteRouter=require('./routes/agenteRouter');
+var estudianteRouter = require('./routes/estudianteRouter');
+var ciudadanoRouter = require('./routes/ciudadanoRouter');
+var comunitarioRouter = require('./routes/comunitarioRouter');
+var agenteRouter = require('./routes/agenteRouter');
 
 var passport = require('passport');
 var authenticate = require('./authenticate');
-
-var mongoose=require('mongoose');
-var config=require('./config');
-var db=mongoose.connect(config.mongoUrl);
-mongoose.connection.on('error',()=>{console.log("Base de datos en problemas")})
-mongoose.connection.once('open',()=>{console.log("Se ha conectado correctamente")})
+var cors = require('cors');
+var mongoose = require('mongoose');
+var config = require('./config');
+var db = mongoose.connect(config.mongoUrl);
+mongoose.connection.on('error', () => { console.log("Base de datos en problemas") })
+mongoose.connection.once('open', () => { console.log("Se ha conectado correctamente") })
 
 
 var app = express();
 
-
+app.use(cors());
 
 // view engine setup bbbb
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.all('*', (req,res,next) =>
-{
-  if(req.secure)
-  {
-    return next;
-  }
-  
-else
-{
-  res.redirect(301,'http://'+req.hostname + req.url);
-}
-}
+app.all('*', (req, res, next) => {
+        if (req.secure) {
+            return next;
+        } else {
+            res.redirect(301, 'http://' + req.hostname + req.url);
+        }
+    }
 
 )
 
@@ -55,10 +50,10 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/estudiante',estudianteRouter);
-app.use('/ciudadano',ciudadanoRouter);
-app.use('/comunitario',comunitarioRouter);
-app.use('/agente',agenteRouter);
+app.use('/estudiante', estudianteRouter);
+app.use('/ciudadano', ciudadanoRouter);
+app.use('/comunitario', comunitarioRouter);
+app.use('/agente', agenteRouter);
 
 
 
@@ -66,19 +61,19 @@ app.use('/agente',agenteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  // res.render('error');
-  res.json(err);
+    // render the error page
+    res.status(err.status || 500);
+    // res.render('error');
+    res.json(err);
 });
 
 module.exports = app;
